8f5fd681b5784e7754c23718591e8d0701bffd2b
Ice Lam <pinky.yy.lam@gmail.com>
2022-02-04 16:40:52 +0800

    feat: update get player data logic in response to change on anime1.me (#9)

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -9,7 +9,6 @@ import responses
 from anime1download.cli.scraper import (
     get_search_result,
     extract_search_results,
-    get_player_url,
     get_player_data,
     get_video_stream
 )
@@ -28,10 +27,8 @@ search_result_paginatated_mock_response_2 = get_mock_response('search-result-mul
 empty_search_result_mock_response = get_mock_response('search-result-empty.html')
 season_detail_mock_response = get_mock_response('season-detail.html')
 video1_detail_mock_response = get_mock_response(os.path.join('video1', 'video-detail.html'))
-player1_mock_response = get_mock_response(os.path.join('video1', 'player.html'))
 player1_api_mock_response = get_mock_response(os.path.join('video1', 'player-api.json'))
 video2_detail_mock_response = get_mock_response(os.path.join('video2', 'video-detail.html'))
-player2_mock_response = get_mock_response(os.path.join('video2', 'player.html'))
 player_api_failed_response = get_mock_response(os.path.join('player-api-invalid-signature.json'))
 
 class TestGetSearchResult(unittest.TestCase):
@@ -244,6 +241,7 @@ class TestExtractSearchResult(unittest.TestCase):
                 ]
             }
         )
+
     def test_extract_search_results_paginated(self):
         """Test that it return anime list and pagination info from HTML string"""
         result = extract_search_results(search_result_paginatated_mock_response_1)
@@ -325,13 +323,14 @@ class TestExtractSearchResult(unittest.TestCase):
                 ]
             }
         )
+
     def test_extract_search_results_without_match(self):
         """Test that it can handles search results without match"""
         result = extract_search_results(empty_search_result_mock_response)
         self.assertEqual(result, { 'previous_page_url': None, 'animes_info': [] })
 
-class TestGetPlayerUrl(unittest.TestCase):
-    """Test for get_player_url()"""
+class TestGetPlayerData(unittest.TestCase):
+    """Test for get_player_data()"""
     def setUp(self):
         """Configure test and mock request response"""
         responses.add(
@@ -349,48 +348,18 @@ class TestGetPlayerUrl(unittest.TestCase):
         )
 
     @responses.activate
-    def test_get_player_url(self):
-        """Test that it returns player url if play button is found"""
-        result = get_player_url('https://anime1.me/6965')
-        self.assertEqual(result, 'https://v.anime1.me/watch?v=tS8VG')
-
-    @responses.activate
-    def test_get_player_url_without_play_button(self):
-        """Test that it returns None for page without play button"""
-        result = get_player_url('https://anime1.me/2018%e5%b9%b4%e7%a7%8b%e5%ad%a3%e6%96%b0%e7%95%aa')
-        self.assertIsNone(result)
-
-class TestGetPlayerData(unittest.TestCase):
-    """Test for get_player_data()"""
-    def setUp(self):
-        """Configure test and mock request response"""
-        responses.add(
-            responses.GET,
-            'https://v.anime1.me/watch?v=tS8VG',
-            body=player1_mock_response,
-            status=200
-        )
-
-        responses.add(
-            responses.GET,
-            'https://anime1.me/2018%e5%b9%b4%e7%a7%8b%e5%ad%a3%e6%96%b0%e7%95%aa',
-            body=season_detail_mock_response,
-            status=200
-        )
-
-    @responses.activate
     def test_get_player_data(self):
-        """Test that it returns player url if play button is found"""
-        result = get_player_data('https://v.anime1.me/watch?v=tS8VG')
+        """Test that it returns player data if video detail page url is valid"""
+        result = get_player_data('https://anime1.me/6965')
         self.assertEqual(
             result,
-            '{"c":"450","e":"1","t":1641037437,"p":0,"s":"70a6a7cffc009b1599de0009822b400c"}'
+            '{"c":"450","e":"1","t":1643960437,"p":0,"s":"2b0446ceb4900c21f663e1c7069da249"}'
         )
 
     @responses.activate
     def test_get_player_data_exception_handling(self):
-        """Test that it returns player url if play button is found"""
-        result = get_player_url('https://anime1.me/2018%e5%b9%b4%e7%a7%8b%e5%ad%a3%e6%96%b0%e7%95%aa')
+        """Test that it returns player data if video detail page url is invalid"""
+        result = get_player_data('https://anime1.me/2018%e5%b9%b4%e7%a7%8b%e5%ad%a3%e6%96%b0%e7%95%aa')
         self.assertIsNone(result)
 
 class TestGetVideoStream(unittest.TestCase):
@@ -406,19 +375,12 @@ class TestGetVideoStream(unittest.TestCase):
         )
 
         responses.add(
-            responses.GET,
-            'https://v.anime1.me/watch?v=tS8VG',
-            body=player1_mock_response,
-            status=200
-        )
-
-        responses.add(
             responses.POST,
             'https://v.anime1.me/api',
             body=player1_api_mock_response,
             match=[
                 responses.matchers.urlencoded_params_matcher({
-                    'd': '{"c":"450","e":"1","t":1641037437,"p":0,"s":"70a6a7cffc009b1599de0009822b400c"}'
+                    'd': '{"c":"450","e":"1","t":1643960437,"p":0,"s":"2b0446ceb4900c21f663e1c7069da249"}'
                 })
             ],
             status=200
@@ -441,19 +403,12 @@ class TestGetVideoStream(unittest.TestCase):
         )
 
         responses.add(
-            responses.GET,
-            'https://v.anime1.me/watch?v=_bBHU',
-            body=player2_mock_response,
-            status=200
-        )
-
-        responses.add(
             responses.POST,
             'https://v.anime1.me/api',
             body=player_api_failed_response,
             match=[
                 responses.matchers.urlencoded_params_matcher({
-                    'd': '{"c":"403","e":"3","t":1641042078,"p":0,"s":"b7818f6857e44d303e9f0f3b37e70194"}'
+                    'd': '{"c":"403","e":"3","t":1643960483,"p":0,"s":"c3a62d6250b216143f80419541e735b5"}'
                 })
             ],
             status=403
@@ -471,8 +426,7 @@ class TestGetVideoStream(unittest.TestCase):
     def test_get_video_stream(self):
         """Test that it returns video stream with file name and file size"""
         result = get_video_stream('https://anime1.me/6965')
-        self.assertEqual(result['player_url'], 'https://v.anime1.me/watch?v=tS8VG')
-        self.assertEqual(result['player_data'], '{"c":"450","e":"1","t":1641037437,"p":0,"s":"70a6a7cffc009b1599de0009822b400c"}')
+        self.assertEqual(result['player_data'], '{"c":"450","e":"1","t":1643960437,"p":0,"s":"2b0446ceb4900c21f663e1c7069da249"}')
         self.assertEqual(result['player_api_response']['l'], '//pekora.v.anime1.me/450/1.mp4')
         self.assertEqual(result['stream'].status_code, 200)
         self.assertEqual(result['file_name'], '1.mp4')
@@ -482,8 +436,7 @@ class TestGetVideoStream(unittest.TestCase):
     def test_get_video_stream_invalid_video_signature(self):
         """Test that it return None when video signature is invalid"""
         result = get_video_stream('https://anime1.me/6966')
-        self.assertEqual(result['player_url'], 'https://v.anime1.me/watch?v=_bBHU')
-        self.assertEqual(result['player_data'], '{"c":"403","e":"3","t":1641042078,"p":0,"s":"b7818f6857e44d303e9f0f3b37e70194"}')
+        self.assertEqual(result['player_data'], '{"c":"403","e":"3","t":1643960483,"p":0,"s":"c3a62d6250b216143f80419541e735b5"}')
         self.assertEqual(result['player_api_response']['success'], False)
         self.assertEqual(result['player_api_response']['errors'], ['Signature invalid.'])
         self.assertEqual(result['stream'], None)
@@ -494,7 +447,6 @@ class TestGetVideoStream(unittest.TestCase):
     def test_get_video_stream_invalid_video_detail_url(self):
         """Test that it return None when video detail url is invalid"""
         result = get_video_stream('https://anime1.me/2018%e5%b9%b4%e7%a7%8b%e5%ad%a3%e6%96%b0%e7%95%aa')
-        self.assertEqual(result['player_url'], None)
         self.assertEqual(result['player_data'], None)
         self.assertEqual(result['player_api_response'], None)
         self.assertEqual(result['stream'], None)
